e7030652871654afb2ed9a75b84f88bc5485d570
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-02-02 18:02:29 +0530

    added js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-// !! IMPORTANT README:
+
+const projectName = 'product-landing-page';
+localStorage.setItem('example_project', 'Product Landing Page');// !! IMPORTANT README:
 
 // You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
 
